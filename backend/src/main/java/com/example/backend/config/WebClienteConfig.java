8f664d0eac99cad53628e5495bf5c68f7f0cf6e2
ValmirboJr <valmirborgesojr@gmail.com>
2025-09-12 23:57:31 -0300

    Feat:Criação da Camada Responsável pela configuração do prompt

--- backend/src/main/java/com/example/backend/config/WebClienteConfig.java
+++ backend/src/main/java/com/example/backend/config/WebClienteConfig.java
@@ -1,4 +1,19 @@
 package com.example.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClienteConfig {
+
+    @Value("${chatgpt.api.url:https://api.openai.com/v1/responses}")
+    private String chatGptApiUrl;
+
+    @Bean
+    public WebClient webClient(WebClient.Builder build){
+        return build.baseUrl(chatGptApiUrl).build();
+    }
+
 }
